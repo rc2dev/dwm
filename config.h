0fe9bd5e4d571d15f36c2932c54573c8e5094459
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-07-27 22:22:26 -0300

    Use bottom bar

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const int user_bh            = 20;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Ubuntu Mono:size=10" };
 
