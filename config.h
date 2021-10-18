7e6e9146c12e4b195965a6fc60aa572fac68cb80
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-10-18 17:04:11 -0300

    Fix formatting

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static const Rule rules[] = {
 
 	/* Swallow */
 	{ "Alacritty", NULL,      NULL,       0,            0,           0,          1,          0,         -1 },
-	{ "St",        NULL,    NULL,       0,            0,           0,          1,          0,         -1 },
+	{ "St",        NULL,      NULL,       0,            0,           0,          1,          0,         -1 },
 	{ NULL,       NULL,       "Event Tester", 0,        0,           0,          0,          1,         -1 },
 	/* VIM shouldn't be swallowed when opening links. */
 	{ "Brave-browser", NULL,  NULL,       0,            0,           0,          0,          1,         -1 },
