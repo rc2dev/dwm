9c87310d1dcbba2fadcaec09662b271f821e580d
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-10-18 17:03:06 -0300

    Don't swallow on starting Brave
    Opening URLs from VIM would swallow VIM.

--- config.h
+++ config.h
@@ -71,6 +71,8 @@ static const Rule rules[] = {
 	{ "Alacritty", NULL,      NULL,       0,            0,           0,          1,          0,         -1 },
 	{ "St",        NULL,    NULL,       0,            0,           0,          1,          0,         -1 },
 	{ NULL,       NULL,       "Event Tester", 0,        0,           0,          0,          1,         -1 },
+	/* VIM shouldn't be swallowed when opening links. */
+	{ "Brave-browser", NULL,  NULL,       0,            0,           0,          0,          1,         -1 },
 
 	/* Tags */
 	{ "St",       "sys",      NULL,       1 << 6,       0,           0,          0,          0,         -1 },
