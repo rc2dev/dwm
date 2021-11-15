edac661b3c242a80b37640fc55f64167606ccf34
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-11-15 11:53:01 -0300

    Update window rules

--- config.h
+++ config.h
@@ -61,6 +61,7 @@ static const Rule rules[] = {
 	{ "Blueman-manager", NULL, NULL,      0,            1,           0,          0,          0,         -1 },
 	{ "Nm-connection-editor", NULL, NULL,      0,            1,           0,          0,          0,         -1 },
 	{ "flameshot", NULL,      NULL,       0,            1,           0,          0,          0,         -1 },
+	{ "photoshop.exe", NULL,      NULL,       0,            1,           0,          0,          0,         -1 },
 
 	/* Floating and sticky */
 	{ "mpv",      NULL,       NULL,       0,            1,           1,          0,          0,         -1 },
