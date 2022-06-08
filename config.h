887a04e960c33d1092765b831a375c9d2ae8458f
Rafael Cavalcanti <dev@rafaelc.org>
2022-06-08 00:28:39 -0300

    Update window rules

--- config.h
+++ config.h
@@ -90,7 +90,7 @@ static const Rule rules[] = {
 	{ "Thunderbird",           NULL,      NULL,                    1 << 6,    0,          0,        0,           0,        -1 },
 	{ "St",                    "cmus",    NULL,                    1 << 7,    0,          0,        0,           0,        -1 },
 	{ "Spotify",               NULL,      NULL,                    1 << 7,    0,          0,        0,           0,        -1 },
-	{ NULL,                    "de.haeckerfelix.Shortwave", NULL,  1 << 7,    0,          0,        0,           0,        -1 },
+	{ "shortwave",             NULL,      NULL,                    1 << 7,    0,          0,        0,           0,        -1 },
 	{ "St",                    "sys",     NULL,                    1 << 8,    0,          0,        0,           0,        -1 },
 };
 
