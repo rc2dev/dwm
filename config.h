ca44f40a64778fe6387d3359ea5a035795adced3
Rafael Cavalcanti <dev@rafaelc.org>
2022-07-03 18:28:47 -0300

    Update keybinding

--- config.h
+++ config.h
@@ -185,7 +185,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_bracketleft,          incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.05} },
-	{ Mod1Mask,                     XK_apostrophe,           zoom,           {0} },
+	{ MODKEY,                       XK_space,                zoom,           {0} },
 	{ MODKEY,                       XK_apostrophe,           view,           {0} },
 	{ MODKEY,                       XK_q,                    killclient,     {0} },
 	{ MODKEY,                       XK_comma,                cyclelayout,    {.i = +1 } },
