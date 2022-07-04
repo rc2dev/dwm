b0d17a0a7921b4a423bc884080ae137abb8df1e6
Rafael Cavalcanti <dev@rafaelc.org>
2022-07-03 22:31:19 -0300

    Update window rules

--- config.h
+++ config.h
@@ -80,7 +80,6 @@ static const Rule rules[] = {
 	{ "Brave-browser",         NULL,      NULL,                    1 << 0,    0,          0,        0,          1,         -1 },
 
 	/* Tags */
-	{ "Chromium-browser",      NULL,      NULL,                    1 << 0,    0,          0,        0,           0,        -1 },
 	{ "Virt-manager",          NULL,      NULL,                    1 << 5,    0,          0,        0,           0,        -1 },
 	{ "Virt-viewer",           NULL,      NULL,                    1 << 5,    0,          0,        0,           0,        -1 },
 	{ "TelegramDesktop",       NULL,      NULL,                    1 << 6,    0,          0,        0,           0,        -1 },
