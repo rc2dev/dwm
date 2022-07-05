53a312b4747b65f124a6a31f7e7945997ca07a12
Rafael Cavalcanti <dev@rafaelc.org>
2022-07-05 14:27:11 -0300

    Remove "go to master" keybinding

--- config.h
+++ config.h
@@ -128,7 +128,7 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	{ MOD, XK_g,     ACTION##stack, {.i = 0 } }, \
+	//{ MOD, XK_g,     ACTION##stack, {.i = 0 } }, \
 	//{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
 	//{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
 	//{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
