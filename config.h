b980993cd15ef3cf7fa1a9945557e637be946d60
Rafael Cavalcanti <dev@rafaelc.org>
2023-03-29 00:07:09 -0300

    Update mouse bindings

--- config.h
+++ config.h
@@ -297,11 +297,12 @@ static Button buttons[] = {
 	/* click                event mask      button          function           argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,         {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,         {.v = &layouts[1]} },
-	{ ClkLtSymbol,          0,              Button2,        killclient,        {0} },
 	{ ClkWinTitle,          0,              Button1,        zoom,              {0} },
+	{ ClkWinTitle,          0,              Button2,        killclient,        {0} },
 	{ ClkWinTitle,          0,              Button4,        focusstack,        {.i = INC(-1) } },
 	{ ClkWinTitle,          0,              Button5,        focusstack,        {.i = INC(+1) } },
 	{ ClkStatusText,        0,              Button1,        spawn,             SHCMD("zenity --calendar") },
+	{ ClkStatusText,        0,              Button2,        spawn,             SHCMD("$TERMINAL -e htop") },
 	{ ClkStatusText,        0,              Button9,        spawn,             {.v = player_next } },
 	{ ClkStatusText,        0,              Button8,        spawn,             {.v = player_prev } },
 	{ ClkStatusText,        0,              Button3,        spawn,             {.v = player_pause } },
