9bdcfead6a19d9199f129724292ddd969d37d75a
Rafael Cavalcanti <dev@rafaelc.org>
2023-04-11 14:25:50 -0300

    Replace tags icons with numbers

--- config.h
+++ config.h
@@ -50,15 +50,15 @@ static char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = {
-	" ",
-	" ",
-	" ",
-	" ",
-	" ",
-	" ",
-	" ",
-	" ",
-	" "
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
 };
 
 static const Rule rules[] = {
