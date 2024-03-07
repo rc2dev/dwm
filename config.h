f4b8c98406db63eb8d70b2e52849497d92b27839
Rafael Cavalcanti <dev@rafaelc.org>
2024-03-07 14:03:33 -0300

    Update keybindings

--- config.h
+++ config.h
@@ -196,7 +196,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,                    zoom,           {0} },
-	{ MODKEY,                       XK_space,                view,           {0} },
+	//{ MODKEY,                       XK_space,                view,           {0} },
 	{ MODKEY,                       XK_q,                    killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_l,                    cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_space,                togglefloating, {0} },
